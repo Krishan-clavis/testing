9944ed10036b77716f5cdd0186dc90ae717f558f
Krishan-clavis <krishan@clavis-tech.com>
2026-07-08 10:34:22 +0530

    Update application status message in server response
    mini change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send(`
     <h1>🚀 Node.js Application</h1>
-    <p>Application is running successfully!</p>
+    <p>Application is running successfully Ready to Deploy!</p>
     <p>Port: ${PORT}</p>
   `);
 });
